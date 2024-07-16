7f7e31a1e31fb758b48717d48c0d6d2bfc5c7be7
reza akbari <rezaakbariiosdeveloper@gmail.com>
2024-07-16 15:15:26 +0330

    improve for chunk chunk fetch data

--- Sources/App/worker/LoadEphemeris.swift
+++ Sources/App/worker/LoadEphemeris.swift
@@ -10,68 +10,69 @@ import Vapor
 import SwiftSoup
 
 class LoadEphemeris {
-    let chunkSize = 1000
-
-    var counter = 0.0
-    var jsonFailedURL: [URL] = []
-    var planetResult: [Planet] = []
-
+    let chunkSize = 50
     let db: Database
 
     init(db: Database) {
         self.db = db
     }
 
-    func generateMinuteTimestamps() async throws -> [Date] {
-        var timestamps: [Date] = []
-
-        let calendar = Calendar.current
-//        let startDateComponents = DateComponents(year: 1999, month: 12, day: 31, hour: 0, minute: 0)
-        let startDateComponents = DateComponents(year: 2024, month: 07, day: 15, hour: 16, minute: 15)
+    func generateMinuteTimestamps() async throws -> AsyncStream<Date> {
+        var calendar = Calendar.current
+        calendar.locale = Locale(identifier: "en-US")
+        calendar.timeZone = .init(secondsFromGMT: .zero)!
+        let startDateComponents = DateComponents(year: 2009, month: 12, day: 31, hour: 0, minute: 0)
         let startDate = calendar.date(from: startDateComponents)!
         let currentDate = Date()
 
-        let oneMinute: TimeInterval = 60
-        let totalMinutes = Int(currentDate.timeIntervalSince(startDate) / oneMinute)
+        return AsyncStream { continuation in
+            Task {
+                let oneMinute: TimeInterval = 60
+                var date = startDate
 
-        await withTaskGroup(of: [Date].self) { group in
-            let numberOfChunks = (totalMinutes / chunkSize) + 1
-
-            for i in 0..<numberOfChunks {
-                group.addTask { [self] in
-                    var chunkTimestamps: [Date] = []
-                    var date = startDate.addingTimeInterval(TimeInterval(i * chunkSize * Int(oneMinute)))
-
-                    for _ in 0..<chunkSize {
-                        if date > currentDate { break }
-                        chunkTimestamps.append(date)
-                        date = date.addingTimeInterval(oneMinute)
-                    }
-
-                    return chunkTimestamps
+                while date <= currentDate {
+                    continuation.yield(date)
+                    date = date.addingTimeInterval(oneMinute)
                 }
-            }
 
-            for await chunk in group {
-                timestamps.append(contentsOf: chunk)
+                continuation.finish()
             }
         }
-        
-        let savedTimestamp = try await savedTimeStamp()
-        
-        return Array(Set(timestamps).subtracting(savedTimestamp))
     }
 
-    func savedTimeStamp() async throws -> [Date] {
-        try await Planet.query(on: db)
+    func savedTimeStamp() async throws -> Set<Date> {
+        let timestamps = try await Planet.query(on: db)
             .all(\.$date)
             .sorted()
+        return Set(timestamps)
     }
-    
-    func loadEphemerisAndConjuction() async throws -> HTTPStatus {
-        var urls: [Date: String] = [:]
 
-        for date in try await generateMinuteTimestamps() {
+    func loadEphemerisAndConjuction() async throws -> HTTPStatus {
+        let savedTimestamp = try await savedTimeStamp()
+        let asyncTimestamps = try await generateMinuteTimestamps()
+        var chunk: [Date] = []
+
+        for await date in asyncTimestamps {
+            if !savedTimestamp.contains(date) {
+                chunk.append(date)
+                if chunk.count >= chunkSize {
+                    try await processChunk(chunk)
+                    chunk.removeAll()
+                }
+            }
+        }
+
+        if !chunk.isEmpty {
+            try await processChunk(chunk)
+        }
+
+        return .ok
+    }
+
+    private func processChunk(_ timestamps: [Date]) async throws {
+        var urls: [Date: String] = [:]
+        
+        for date in timestamps {
             urls[date] =
                 "https://horoscopes.astro-seek.com/browse-current-planets/?datum_interval=\(date)&styl_graf=1&narozeni_mesto=&nastaveni_toggle=&aspekty_detail_check=1&orb=0&house_system=none&phours=&hid_fortune_check=on&hid_vertex_check=on&hid_chiron_check=on&hid_lilith_check=on&hid_uzel_check=on&interval_smer=zpet&interval_hodnota=1day&aya="
         }
@@ -79,15 +80,11 @@ class LoadEphemeris {
         let totalCount: CGFloat = CGFloat(urls.count)
         try await withThrowingTaskGroup(of: [Planet].self) { group in
             for (date, urlString) in urls {
-                group.addTask { [self] in
+                group.addTask {
                     guard let url = URL(string: urlString) else {
                         throw Abort(.forbidden)
                     }
-                    let (content, failedURL) = await url.fetchContents()
-
-                    if let failedURL {
-                        jsonFailedURL.append(failedURL)
-                    }
+                    let (content, _) = await url.fetchContents()
                     guard let content else { throw Abort(.forbidden) }
 
                     do {
@@ -104,7 +101,6 @@ class LoadEphemeris {
                             let (degree, minutes) = try degreeAndMinutesColumns[index].text().extractDegreeAndMinuets()
                             let rx = try !rxColumns[index].text().trimmingCharacters(in: .whitespacesAndNewlines).isEmpty
 
-                            
                             planetResult.append(Planet(
                                 id: "\(date)" + "-" + planet + "-" + zodiac + "-" + degree + "-" + minutes + "-" + "\(rx)",
                                 date: date,
@@ -116,23 +112,20 @@ class LoadEphemeris {
                             ))
                         }
 
-                        counter += 1
-                        print(counter / totalCount * 100)
+                        print(date)
                         return planetResult
                     } catch {
                         print("Failed to parse content for URL: \(url) with error: \(error)")
                         throw Abort(.forbidden)
                     }
                 }
+            }
 
-                while let planet = try await group.next() {
-                    try await planet.asyncForEach {
-                        try await $0.save(on: db)
-                    }
+            for try await planets in group {
+                for planet in planets {
+                    try await planet.save(on: db)
                 }
             }
         }
-
-        return .ok
     }
 }
